35bd9c0931105633cbc14fe8192f5a74c584344a
diskman88 <diskman88@163.com>
2024-10-16 19:25:08 +0800

    Changes to be committed: 	modified:   port/adf_components/audio_board/CMakeLists.txt 	new file:   port/adf_components/audio_board/labplus_Ledong_psram/board.c 	new file:   port/adf_components/audio_board/labplus_Ledong_psram/board.h 	new file:   port/adf_components/audio_board/labplus_Ledong_psram/board_def.h 	new file:   port/adf_components/audio_board/labplus_Ledong_psram/board_pins_config.c 	new file:   port/boards/labplus_Ledong_psram/audio/CMakeLists.txt 	new file:   port/boards/labplus_Ledong_psram/audio/board_def.h 	new file:   port/boards/labplus_Ledong_psram/audio/board_init.c 	new file:   port/boards/labplus_Ledong_psram/audio/board_init.h 	new file:   port/boards/labplus_Ledong_psram/audio/board_pins_config.c 	new file:   port/boards/labplus_Ledong_psram/audio/board_pins_config.h 	new file:   port/boards/labplus_Ledong_psram/board.json 	new file:   port/boards/labplus_Ledong_psram/board.md 	new file:   port/boards/labplus_Ledong_psram/main.c 	new file:   port/boards/labplus_Ledong_psram/manifest.py 	new file:   port/boards/labplus_Ledong_psram/modules/_boot.py 	new file:   port/boards/labplus_Ledong_psram/modules/calibrate_img.py 	new file:   port/boards/labplus_Ledong_psram/modules/camera.py 	new file:   port/boards/labplus_Ledong_psram/modules/educore/__init__.py 	new file:   port/boards/labplus_Ledong_psram/modules/educore/_ble.py 	new file:   port/boards/labplus_Ledong_psram/modules/educore/_camera1956.py 	new file:   port/boards/labplus_Ledong_psram/modules/educore/_educore.py 	new file:   port/boards/labplus_Ledong_psram/modules/educore/_smartcamera.py 	new file:   port/boards/labplus_Ledong_psram/modules/educore/k210/__init__.py 	new file:   port/boards/labplus_Ledong_psram/modules/educore/k210/ai.py 	new file:   port/boards/labplus_Ledong_psram/modules/educore/k210/asr.py 	new file:   port/boards/labplus_Ledong_psram/modules/educore/k210/public.py 	new file:   port/boards/labplus_Ledong_psram/modules/mpython.py 	new file:   port/boards/labplus_Ledong_psram/modules/v831/__init__.py 	new file:   port/boards/labplus_Ledong_psram/modules/v831/ai.py 	new file:   port/boards/labplus_Ledong_psram/modules/v831/demo.py 	new file:   port/boards/labplus_Ledong_psram/modules/v831/label_1000classes.py 	new file:   port/boards/labplus_Ledong_psram/modules/v831/public.py 	new file:   port/boards/labplus_Ledong_psram/mpconfigboard.cmake 	new file:   port/boards/labplus_Ledong_psram/mpconfigboard.h 	new file:   port/boards/labplus_Ledong_psram/partitions-8MiB.csv 	new file:   port/boards/labplus_Ledong_psram/sdkconfig.board 	new file:   port/boards/labplus_Ledong_psram/sdkconfig.spiram 	modified:   port/main_esp32/Kconfig.projbuild

---
+++ port/boards/labplus_Ledong_psram/mpconfigboard.cmake
@@ -0,0 +1,74 @@
+# 本文件会被顶层CMakeList.txt包含
+# 可以在本文件定义一些板级源文件及目录
+
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    ${SDKCONFIG_IDF_VERSION_SPECIFIC}
+    boards/sdkconfig.ble 
+    boards/sdkconfig.240mhz
+    boards/labplus_Ledong_psram/sdkconfig.spiram 
+    boards/labplus_Ledong_psram/sdkconfig.board
+)
+
+if(NOT MPY_PORT_DIR)
+    get_filename_component(MPY_PORT_DIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
+endif()
+
+if(NOT ADF_PATH)
+    get_filename_component(ADF_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../esp-adf ABSOLUTE)
+endif()
+
+set(ADF_COMPS ${ADF_PATH}/components)
+
+set(MICROPY_SOURCE_BOARD
+    ${MICROPY_BOARD_DIR}/main.c
+    # ${MPY_PORT_DIR}/drivers/startup/00030.c
+    # ${MPY_PORT_DIR}/drivers/startup/i2c_master.c
+    # ${MPY_PORT_DIR}/drivers/startup/oled.c
+    # ${MPY_PORT_DIR}/drivers/startup/startup.c 
+    ${MPY_PORT_DIR}/lib/utils/pyexec.c
+    ${MPY_PORT_DIR}/builtins/modmusictunes.c
+    ${MPY_PORT_DIR}/builtins/modmusic.c
+    ${MPY_PORT_DIR}/builtins/esp32_nvs.c
+    ${MPY_PORT_DIR}/builtins/machine_pin.c
+    ${MPY_PORT_DIR}/builtins/machine_touchpad.c
+    ${MPY_PORT_DIR}/builtins/modframebuf.c
+    ${MPY_PORT_DIR}/builtins/mod_audio/audio_player.c
+    ${MPY_PORT_DIR}/builtins/mod_audio/audio_recorder.c 
+    ${MPY_PORT_DIR}/builtins/mod_audio/vfs_stream.c 
+    ${MPY_PORT_DIR}/builtins/mod_audio/modaudio.c 
+)
+
+set(MICROPY_SOURCE_BOARD_DIR
+    ${MPY_PORT_DIR}/drivers
+    ${MPY_PORT_DIR}/lib
+    ${MPY_PORT_DIR}/builtins
+    ${MPY_PORT_DIR}/boards/labplus_classroom_kit_nanjing/audio
+)
+
+# if(CONFIG_LABPLUS_CLASSROOM_KIT_NANJING_BOARD)
+set(ADF_COMPONENTS 
+    ${MPY_PORT_DIR}/adf_components/adf_utils
+    ${MPY_PORT_DIR}/adf_components/audio_board
+    ${MPY_PORT_DIR}/adf_components/audio_hal
+    ${MPY_PORT_DIR}/adf_components/audio_pipeline
+    ${MPY_PORT_DIR}/adf_components/audio_recorder
+    ${MPY_PORT_DIR}/adf_components/audio_sal
+    ${MPY_PORT_DIR}/adf_components/audio_stream
+    ${MPY_PORT_DIR}/adf_components/clouds
+    ${MPY_PORT_DIR}/adf_components/display_service
+    ${MPY_PORT_DIR}/adf_components/dueros_service
+    ${MPY_PORT_DIR}/adf_components/esp_actions
+    ${MPY_PORT_DIR}/adf_components/esp_dispatcher
+    ${MPY_PORT_DIR}/adf_components/esp_peripherals
+    ${MPY_PORT_DIR}/adf_components/esp-adf-libs
+    ${MPY_PORT_DIR}/adf_components/esp-sr 
+    ${MPY_PORT_DIR}/adf_components/tone_partition 
+    ${MPY_PORT_DIR}/adf_components/wifi_service)
+# endif()
+
+list(APPEND EXTRA_COMPONENT_DIRS
+    ${ADF_COMPONENTS}
+    ${MPY_PORT_DIR}/boards/labplus_classroom_kit_nanjing/audio)
+
+set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
---
+++ port/boards/labplus_Ledong_psram/mpconfigboard.h
@@ -0,0 +1,13 @@
+#define MICROPY_HW_BOARD_NAME "CLASSROOM KIT"
+#define MICROPY_HW_MCU_NAME "ESP32"
+#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT "mpy-classroom-kit"
+
+#ifndef MICROPY_PY_ESP_MUSIC
+#define MICROPY_PY_ESP_MUSIC                   (1)
+#endif
+
+#define MICROPY_HW_ENABLE_GPIO16 (1)
+#define MICROPY_HW_ENABLE_GPIO17 (1)
+
+// #define MICROPY_PY_TTS                         (1)
+// #define MICROPY_PY_ASR                         (1)
